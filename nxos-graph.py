e19024af97e45847370bcc27c4a62a0325bb2c48
mahesrin <mahesrin@cisco.com>
2016-07-18 15:43:28 +0530

    reached a stable point

--- nxos-graph.py
+++ nxos-graph.py
@@ -10,22 +10,43 @@ data = json.load(sys.stdin)
 service_users_dict = {}
 service_providers_dict = {}
 
-scope_colors= {'boot':0x110000,'infra':0x880000,'control':0xcc0001,'':0xff0000}
-mode_colors = {'asap':0x001100,'conditional':0x008800,'on_demand':0x00cc00,'':0x00ff00}
-condition_colors = {'optional':0x000011,'compulsory':0x000088,'':0x0000cc}
- 
+
+colors = { 
+'boot':'red',
+'bootasap':'blue3',
+'asap':'turquoise',
+'controlon_demand':'blue',
+'controlconditional':'blueviolet',
+'controlasap':'green',
+'controlconditionalcompulsory':'limegreen',
+'infraconditional':'forestgreen',
+'infraconditionalcompulsory':'yellow',
+'controlconditionaloptional':'firebrick4',
+'infraasap':'orange',
+'conditionalcompulsory':'orange3',
+'on_demand':'slateblue',
+'infraasapcompulsory':'cyan4'
+}
+
 #generate dot file
 print " digraph G { \ncenter=true;\n rankdir=LR; \nranksep=equally ;\n ratio=fill;\n  size = \"7.5,7.5\";"
+for key in  colors:
+	print key+" [shape=rectangle,color="+colors[key]+",style=filled];"
 for obj in data:
 	service_user =  obj['service_name']
 	service_providers =  obj['service_dependency']
 	service_providers_dict[service_user] = service_providers
-	service_scope = obj["scope"]
-	node_color =  scope_colors[obj['scope']] | mode_colors[obj['run_mode']] | condition_colors[obj['run_condition']] 
-	node_color = str(hex(node_color)) 	
-	node_color = node_color.replace('0x','')	
+	color_key = ''
+	if 'scope' in obj and obj['scope']:
+		color_key = obj['scope']
+	if 'run_mode' in obj and obj['run_mode']:
+		color_key = color_key + obj['run_mode']
+	if 'run_condition' in obj and obj['run_condition']:
+		color_key = color_key + obj['run_condition']
+	print color_key
+	node_color = colors[color_key]
 	service_user = service_user.replace('-','_')
-	print  service_user+" [label="+service_user+", fontcolor=\"#"+node_color+"\",color=\"#"+node_color+"\"]"
+	print  service_user+" [label="+service_user+", style=filled ,color="+node_color+"]"
 	
 for service_user, service_providers in service_providers_dict.iteritems():
 	service_user = service_user.replace('-','_')
