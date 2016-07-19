96b7ee73a66b65306a19e88ff5960948dc2bcf9d
mahesrin <mahesrin@cisco.com>
2016-07-19 11:06:29 +0530

    added new color keys

--- nxos-graph.py
+++ nxos-graph.py
@@ -14,10 +14,12 @@ service_providers_dict = {}
 colors = { 
 'boot':'red',
 'bootasap':'blue3',
+'bootasapcompulsory':'coral2',
 'asap':'turquoise',
 'controlon_demand':'blue',
 'controlconditional':'blueviolet',
 'controlasap':'green',
+'controlasapcompulsory':'brown',
 'controlconditionalcompulsory':'limegreen',
 'infraconditional':'forestgreen',
 'infraconditionalcompulsory':'yellow',
@@ -29,7 +31,7 @@ colors = {
 }
 
 #generate dot file
-print " digraph G { \ncenter=true;\n rankdir=LR; \nranksep=equally ;\n ratio=fill;\n  size = \"7.5,7.5\";"
+print " digraph nxos_dependency { \ncenter=true;\n rankdir=LR; \nranksep=equally ;\n ratio=fill;\n  size = \"7.5,7.5\";"
 for key in  colors:
 	print key+" [shape=rectangle,color="+colors[key]+",style=filled];"
 for obj in data:
@@ -43,7 +45,6 @@ for obj in data:
 		color_key = color_key + obj['run_mode']
 	if 'run_condition' in obj and obj['run_condition']:
 		color_key = color_key + obj['run_condition']
-	print color_key
 	node_color = colors[color_key]
 	service_user = service_user.replace('-','_')
 	print  service_user+" [label="+service_user+", style=filled ,color="+node_color+"]"
